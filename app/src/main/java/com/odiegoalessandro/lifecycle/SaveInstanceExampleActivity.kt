771868f768c7fc65026e888114c64837f7fd3a42
= <=>
2022-10-14 13:27:18 -0300

    feat: create SaveInstanceExampleActivity

---
+++ app/src/main/java/com/odiegoalessandro/lifecycle/SaveInstanceExampleActivity.kt
@@ -0,0 +1,11 @@
+package com.odiegoalessandro.lifecycle
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SaveInstanceExampleActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_save_instance_example)
+    }
+}
